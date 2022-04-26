0a843f8236e27f6734d02042acf62203985f07f5
DersH22 <andershartmark1@gmail.com>
2022-04-26 01:31:26 -0400

    unfinished updates to homescreen demo

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,8 +7,27 @@ import { RootTabScreenProps } from '../types';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Today's View</Text>
+      </View>
+      
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+
+      <View style={styles.container}>
+        <Image
+          style={styles.weather}
+          source={require('../sample_weather_api.png')}
+        />
+      </View>
+
+      {/*
+      <Image
+        style={styles.weather}
+        source={require('../sample_weather_api.png')}
+      />
+      */}
+
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
@@ -29,4 +48,10 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  weather: {
+    width: 250,
+    height: 350,
+    marginVertical: 10,
+    marginHorizontal: 10,
+  }
 });
