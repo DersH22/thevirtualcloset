bff69753a30dee694580a0f75b181783f37879d9
DersH22 <andershartmark1@gmail.com>
2022-05-02 18:47:33 -0400

    removed camera from AddOutfitScreen.tsx

--- screens/AddOutfitScreen.tsx
+++ screens/AddOutfitScreen.tsx
@@ -1,51 +1,15 @@
-import EditScreenInfo from '../components/EditScreenInfo';
-import React, {useState, useEffect} from 'react';
-import {StyleSheet, TouchableOpacity} from 'react-native';
-import {Camera} from 'expo-camera';
+import { StyleSheet, Image} from 'react-native';
 
+import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
-export default function AddOutfitScreen() {
-
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-
+export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Add Outfit</Text>
       {/*<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />*/}
       {/*<EditScreenInfo path="/screens/TabOneScreen.tsx" />*/}
-
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
     </View>
   );
 }
@@ -70,23 +34,5 @@ const styles = StyleSheet.create({
     height: 350,
     marginVertical: 10,
     marginHorizontal: 10,
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    margin: 20,
-  },
-  button: {
-    flex: 0.1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-    color: 'white',
-  },
+  }
 });
